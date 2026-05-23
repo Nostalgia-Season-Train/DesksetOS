035342c75df67beb03b2e6f7cba06df7b30d7098
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-05-23 15:21:05 +0800

    简化项目结构

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
-import './assets/main.css'
+/* ==== 全局样式 ==== */
+import './main.css'
 
+
+/* ==== 路由 ==== */
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/noname1', name: 'noname1', component: () => import('./noname1/App.vue') }
+  ]
+})
+
+
+/* ==== 应用 ==== */
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import Main from './Main.vue'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+const app = createApp(Main)
+  .use(router)
+  .mount('#app')
