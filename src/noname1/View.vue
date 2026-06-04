c2f7f6b6adad5ca9315993bcab1cc1ac6a8cf96c
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-06-04 13:22:09 +0800

    添加壁纸

--- src/noname1/View.vue
+++ src/noname1/View.vue
@@ -15,7 +15,7 @@ main {
   width: 100%;
   height: 100%;
 
-  background-image: url('./assets/wallpaper.png');  /* 暂不追踪壁纸文件 */
+  background-image: url('./assets/wallpaper.jpg');
   background-position: center;
   background-size: cover;
   background-attachment: fixed;
